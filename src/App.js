1aac9c3e7717b09b449065bb9254d6c0a1fdd16e
stephens2633 <stephens2633@gmail.com>
2020-04-19 16:21:28 -0500

    shop compoenents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/hompage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
